3c482e52b0ff7fe7242f275428530e0b09002e76
Aaron Kocsis <aaronkocsis13@gmail.com>
2024-03-06 14:57:33 -0600

    Changed Folder Structure

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PropertyCardComponent, PropertyListComponent, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'FirstAngularProject';
+}
